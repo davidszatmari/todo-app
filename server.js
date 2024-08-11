5b6effc8835f6bb1a57ecabf5d0292feca640705
David Szatmari <szatmaridavid7@gmail.com>
2024-08-11 19:56:33 +0200

    Post request

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.get("/api/tasks", (req, res) => {
   res.json(tasks);
 });
 
+app.post("/api/tasks", (req, res) => {
+  const { title } = req.body;
+  if (title) {
+    const newTask = { id: tasks.length + 1, title, completed: false };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+  } else {
+    res.status(400).json({ error: "Title is required" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
 });
