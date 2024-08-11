44a4f040b9daeec6c276472ebf04df80917cf0f3
David Szatmari <szatmaridavid7@gmail.com>
2024-08-11 19:45:53 +0200

    Get request

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const PORT = 3000;
 
 let tasks = [];
 
+app.use(express.json());
+
+app.get("/api/tasks", (req, res) => {
+  res.status(200).json;
+  res.json(tasks);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
 });
